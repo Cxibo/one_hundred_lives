a6e4e293c7bbb9c9e45a5f8382a1eacd51ca2fd5
Cxibo <1171393323@qq.com>
2020-03-01 23:00:42 +0800

    hello.js

--- js/js_learning/av80536021/hello.js
+++ js/js_learning/av80536021/hello.js
@@ -1,5 +1,13 @@
-console.log('hello js');
-var a = 10;
-console.log(a);
-var b = 20;
-console.log(b);
+// js解析环节变量提升
+console.log(s);
+var s = 'hello';
+
+function func(){
+    if(false){
+        var web = 'hhl';
+
+    }
+    console.log(web);
+
+}
+func();
